c8151128f1c2aa06c5a61ceca3ce8c8239f2ba4e
larsstrange <lars.strange@gmail.com>
2024-02-24 18:52:27 +0100

    De første filer til MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,26 @@
+basic.forever(function () {
+    basic.showString("Aksel")
+    basic.showString("og")
+    basic.showString("Lars")
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . # . .
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . # . .
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . # . .
+        # . . . #
+        . # # # .
+        `)
+})
